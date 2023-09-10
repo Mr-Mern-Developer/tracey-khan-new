15deebcf27e5160120b9ed9a04fa4ae126a0fc44
RakibHasanDev <rakibkst16@gmail.com>
2023-09-10 23:30:31 +0600

    added some design

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,19 +1,71 @@
 import React from 'react';
 import SideDrawer from './SideDrawer';
-
+import { FaFacebookF, FaTwitter } from 'react-icons/fa';
+import { BsInstagram, BsYoutube } from 'react-icons/bs';
+import { BiSearch } from 'react-icons/bi';
+import logo from "../../../src/assests/logo.png"
+import { Link } from 'react-router-dom';
 const NavBar = () => {
     return (
         <div>
-            <div className='bg-[#000000]'>
-                /<div>
 
+            {/* tobar start  */}
+            <div className='bg-[#000000]  py-5 '>
+                <div className='flex w-[90%] mx-auto bg-black '>
+                    <div className='  flex w-[43%]   '>
+                        <div className='text-white flex items-center gap-3  text-xl'>
+                            <FaFacebookF className='w-9 h-9 bg-[rgba(255,255,255,.15)] rounded-full p-2' />
+                            <FaTwitter className='w-9 h-9 bg-[rgba(255,255,255,.15)] rounded-full p-2' />
+                            <BsInstagram className='w-9 h-9 bg-[rgba(255,255,255,.15)] rounded-full p-2' />
+                            <BsYoutube className='w-9 h-9 bg-[rgba(255,255,255,.15)] rounded-full p-2' />
+
+                        </div>
+
+                    </div>
+                    <div className=' flex justify-center'>
+                        <img src={logo}>
+                        </img>
+
+                    </div>
+                </div>
+
+
+            </div>
+
+            {/* topbar end */}
+
+            {/* navbar start */}
+            <div className='w-[90%] mx-auto mt-2 flex items-center justify-between'>
+                <div>
+                    <SideDrawer></SideDrawer>
                 </div>
                 <div>
-                    
+                    <ul className='flex gap-4 uppercase font-bold '>
+                        <Link to='/' className='hover:text-red-600'>
+                            Musics
+                        </Link>
+                        <Link to='/' className='hover:text-red-600'>
+                            News
+                        </Link>
+                        <Link to='/' className='hover:text-red-600'>
+                            Videos
+                        </Link>
+                        <Link to='/' className='hover:text-red-600'>
+                            Reviews
+                        </Link>
+
+                    </ul>
+
+                </div>
+
+                <div>
+                    <BiSearch className='text-black text-2xl' />
                 </div>
 
             </div>
-            <SideDrawer></SideDrawer>
+
+            {/* navbar end  */}
+
         </div>
     );
 };
--- src/Components/NavBar/SideDrawer.js
+++ src/Components/NavBar/SideDrawer.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TbListTree } from 'react-icons/tb';
 
 const SideDrawer = () => {
-    return (
-        <div>
-                        <div className="drawer">
-  <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-  <div className="drawer-content">
-    {/* Page content here */}
-    <label htmlFor="my-drawer" className="btn btn-primary drawer-button">Open drawer</label>
-  </div> 
-  <div className="drawer-side">
-    <label htmlFor="my-drawer" className="drawer-overlay"></label>
-    <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-      {/* Sidebar content here */}
-      <li><a>Sidebar Item 1</a></li>
-      <li><a>Sidebar Item 2</a></li>
-      
-    </ul>
-  </div>
-</div>
-        </div>
-    );
+
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
+  return (
+    <div>
+       <div className={`drawer ${drawerOpen ? 'open' : ''} `}>
+      <input
+        id="my-drawer"
+        type="checkbox"
+        className="drawer-toggle"
+        checked={drawerOpen}
+        onChange={toggleDrawer}
+      />
+      <div className="drawer-content">
+        <label htmlFor="my-drawer" className="text-3xl text-black hover:text-red-600">
+          <TbListTree />
+        </label>
+      </div>
+      <div className="drawer-side  ">
+        <label htmlFor="my-drawer" className="drawer-overlay"></label>
+        <ul className="menu p-4 w-80 min-h-full bg-black text-gray-100 relative">
+          <li><a>Sidebar Item 1</a></li>
+          <li><a>Sidebar Item 2</a></li>
+          <div className=''><button onClick={toggleDrawer} className="text-white mt-4  absolute right-6 top-0 w-9 h-9 bg-[rgba(255,255,255,.15)] rounded-full p-2 text-xl flex items-center justify-center"> x </button></div>
+        </ul>
+        {/* Add a close button here */}
+        
+      </div>
+    </div>
+    </div>
+  );
 };
 
 export default SideDrawer;
